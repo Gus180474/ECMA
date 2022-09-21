c41d01a9da1541fd4711e5045aa25e4357b10837
Gus180474 <gustep_ebc2001@yahoo.com.mx>
2022-09-21 01:36:41 -0500

    guardado de index ES7

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+
+// se pouede utilizar include para sber si se encuentra un valor en un arreglo antes dse hacia con 
+//indexof
+let numbers =[1,2,3,7,8];
+if(numbers.includes(9)){
+    console.log('Si se encuentra el valor 7');
+}else{
+    console.log('No se encuentra')
+}
+// exponente elevar a la potencia con es7
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
+
