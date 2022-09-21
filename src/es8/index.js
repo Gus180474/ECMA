c4026625fa5cf5391d0558236e91ae3568d82fdb
Gus180474 <gustep_ebc2001@yahoo.com.mx>
2022-09-21 02:18:43 -0500

    guardado de index ES9

--- src/es8/index.js
+++ src/es8/index.js
@@ -26,3 +26,28 @@ console.log(string.padEnd(12,' ---------------'));
 const obj = {
     name: 'Gus',// para poder agregar mas datos ??
 }
+// Async Await
+const helloWorld = () => {
+    return new Promise((resolve, reject) =>{
+        (fals)
+        ? setTimeout(() => resolve('Hello World'), 3000)
+        : reject(new Error('Test Error'))
+    })
+};
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+helloAsync();
+// otra
+const anotherFunction = async () => {
+    try{
+        const hello = await helloWorld();
+        console.log(hello);
+    }catch (error){
+        console.log(error);
+    }
+};
+anotherFunction();
+
+
