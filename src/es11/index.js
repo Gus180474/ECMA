0b6e46c3803ae4bec41f31e89429e5bffe20f52c
Gus180474 <gustep_ebc2001@yahoo.com.mx>
2022-09-21 04:15:40 -0500

    guardado de index ES2020

--- src/es11/index.js
+++ src/es11/index.js
@@ -5,3 +5,32 @@ button.addEventListener("click", async function(){
     const module = await import("./file.js");
     module.hello();
 });
+
+// ECMAScript 2020 - características importantes
+const aBigNumber = 9007199254740991n;
+const anotherBigNumber = BigInt(9007199254740991);
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
+// Manejo de varias promesas
+const promise1 = new Promise((resolve, reject) => reject("reject"));
+const promise2 = new Promise((resolve, reject) => resolve("resolve"));
+const promise3 = new Promise((resolve, reject) => resolve("resolve 1"));
+
+Promise.allSettled([promise1, promise2,promise3])
+    .then(response => console.log(response));
+
+//Objeto global para cualquier plataforma
+console.log(window);
+console.log(globalThis);
+//Operador Nullish Coalescing
+const fooo= null ?? 'default string';
+console.log(fooo);
+//Encadenamiento opcional 
+const user = {};
+console.log(user?.profile?.email);
+if(user?.profile?.email){
+    console.log('email')
+}else {
+    console.log('fail');
+}
